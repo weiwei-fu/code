e51d153dcfa9f2f845f8dbd9dc336ab3d4e44e64
fuweiwei <weiwei.fu001@gmail.com>
2018-11-02 14:53:06 +0800

    加入zookeeper的创建连接和关闭连接的信息

---
+++ src/main/java/com/fww/code/zk/ZookeeperConnection.java
@@ -0,0 +1,32 @@
+package com.fww.code.zk;
+
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by @author fww on 2018/11/2.
+ */
+public class ZookeeperConnection {
+
+    private ZooKeeper zooKeeper;
+
+    final CountDownLatch connectedSignal = new CountDownLatch(1);
+
+    public ZooKeeper connect(String host) throws InterruptedException, IOException {
+        zooKeeper = new ZooKeeper(host, 500, watchedEvent -> {
+            if (watchedEvent.getState()== Watcher.Event.KeeperState.SyncConnected) {
+                connectedSignal.countDown();
+            }
+        });
+        connectedSignal.await();
+        return zooKeeper;
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+}
